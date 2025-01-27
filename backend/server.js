1e32253d4857f9425b7c3cd3d6f7e794750c37bf
psiconervio <psiconervio@gmail.com>
2025-01-27 08:31:13 -0300

    asdads

--- backend/server.js
+++ backend/server.js
@@ -1,58 +1,23 @@
 const express = require('express');
-const prisma = require('@prisma/client').PrismaClient;
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// Inicializa Prisma Client
-const prismaClient = new prisma();
-
-// Middleware para parsear JSON
+// Middleware global
 app.use(express.json());
+app.use(cors());
 
-// Ruta GET: Obtener todos los ingredientes
-app.get('/ingredients', async (req, res) => {
-  try {
-    const ingredients = await prismaClient.ingredient.findMany();
-    res.json(ingredients);
-  } catch (error) {
-    res.status(500).json({ error: 'Error al obtener ingredientes' });
-  }
-});
+// Rutas
+const ingredientRoutes = require('./routes/ingredients');
+const userRoutes = require('./routes/users');
 
-// Ruta POST: Crear un nuevo ingrediente
-app.post('/ingredients', async (req, res) => {
-  const { name, unit, price, quantity } = req.body;
-  try {
-    const newIngredient = await prismaClient.ingredient.create({
-      data: {
-        name,
-        unit,
-        price,
-        quantity,
-      },
-    });
-    res.json(newIngredient);
-  } catch (error) {
-    res.status(500).json({ error: 'Error al crear ingrediente' });
-  }
-});
+// Usar las rutas de ingredientes
+app.use('/api/ingredients', ingredientRoutes);
 
-// Ruta DELETE: Eliminar un ingrediente
-app.delete('/ingredients/:ingredientId', async (req, res) => {
-  const { ingredientId } = req.params;
-  try {
-    const deletedIngredient = await prismaClient.ingredient.delete({
-      where: {
-        id: Number(ingredientId),  // Asegúrate de convertir a número si es necesario
-      },
-    });
-    res.json(deletedIngredient);
-  } catch (error) {
-    res.status(500).json({ error: 'Error al eliminar ingrediente' });
-  }
-});
+// Usar las rutas de usuarios
+app.use('/api/users', userRoutes);
 
-// Iniciar servidor
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
